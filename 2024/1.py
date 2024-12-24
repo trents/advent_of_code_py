4aa786b8ec78381f566eac6e646535fd994a7115
Trent S. <devolver42@gmail.com>
2024-12-24 11:03:09 -0600

    Continued work on Day 1

--- 2024/1.py
+++ 2024/1.py
@@ -1,4 +1,5 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('https://github.com/trents/advent_of_code_py/blob/main/2024/input1.txt', delimiter='  ') 
+df = pd.read_csv('https://raw.githubusercontent.com/trents/advent_of_code_py/refs/heads/main/2024/input1.txt', delimiter=',') 
+print(df)
